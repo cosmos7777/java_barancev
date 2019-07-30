a08e5ecb10cd34eeaa6086129e7f63dbcc822f59
Andrey Gevorkov <cosmos7777@yandex.ru>
2019-07-30 18:49:36 +0300

    Считаем площади 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,15 +6,12 @@ public class MyFirstProgram{
 
       hello("world");
 
-      double l = 5;
-      System.out.println("Площадь квадрата со стороной " + l + "=" + area(l));
+      Square s = new Square(5);
+      System.out.println("Площадь квадрата со стороной " + s.l + "=" + area(s));
 
-      double a =4;
-      double b =6;
-      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " +area(a,b));
+      Rectangle r = new Rectangle(4,6);
+      System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " +area(r));
   }
-
-
 
 
   public static void hello(String somebody) {
@@ -22,11 +19,11 @@ public class MyFirstProgram{
       System.out.println("Hello, " + somebody + "!");
 
   }
-  public static double area(double len){
-      return len*len;
+  public static double area(Square s){
+      return s.l*s.l;
   }
-  public static double area(double a, double b){
-      return a*b;
+  public static double area(Rectangle r){
+      return r.a*r.b;
   }
 
 
