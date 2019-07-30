ea63d9c9f65f07e6e928a477c91789c5ba3fe2a4
Andrey Gevorkov <cosmos7777@yandex.ru>
2019-07-30 18:26:29 +0300

    Считаем площади

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Factorial.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+public class Factorial{
+    static int calculateFactorial(int n){
+        int result = 1;
+        for (int i = 1; i <=n; i ++){
+            result = result*i;
+        }
+        return result;
+
+
+    }
+
+    public static void main(String[] args){
+        System.out.println(calculateFactorial(4));
+    }
+}
